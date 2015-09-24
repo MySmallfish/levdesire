6eef83d36c6f3faa45e1f9b6ceb6c73cf159c094
Yair Cohen <mysmallfish@gmail.com>
2015-09-25 01:43:22 +0300

    Initial upload, includes all (without .gitignore)

---
+++ nopCommerce/Presentation/Nop.Web/Themes/Jewelry/Content/Scripts/Jewelry.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    var responsiveMenuSettings = {
+        doesBackgroundChange: true,
+        doesPaddingChange: true,
+        bgSelector: ".mega-menu-responsive > li > ul, .top-menu > li > .sublist-wrap",
+        bgInitialColor: "rgba(34,34,34,1)",
+        red: 20,
+        green: 20,
+        blue: 20,
+        alpha: 0.05,
+        paddingSelector: ".header-menu > ul > li",
+        paddingValue: 20,
+        themeBreakpoint: 980
+    };
+
+    initResponsiveTheme(responsiveMenuSettings);
+
+});
